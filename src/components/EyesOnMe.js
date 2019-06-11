a5bf176c85020d2216815cea2e2c3ede6e92404d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-06-11 05:51:54 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,18 +1,31 @@
 // Code EyesOnMe Component Here
 import React from 'react';
 
+<<<<<<< HEAD
 export default class EyesOnMe extends React.Component {
   handleFocus = () => {
     console.log('Good!');
   }
 
   handleBlur = () => {
+=======
+class EyesOnMe extends React.Component {
+  handleFocus = (e) => {
+    console.log('Good!');
+  }
+
+  handleBlur = (e) => {
+>>>>>>> a91c6a000985d5dc94b9be59bb01017f8bf27580
     console.log('Hey! Eyes on me!');
   }
 
   render() {
     return (
+<<<<<<< HEAD
         <button onFocus={this.handleFocus} onBlur={this.handleBlur}></button>
+=======
+        <button onFocus={handleFocus} onBlur={handleBlur}></button>
+>>>>>>> a91c6a000985d5dc94b9be59bb01017f8bf27580
     );
   }
 }
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,14 +1,23 @@
 // Code Keypad Component Here
 import React from 'react';
 
+<<<<<<< HEAD
 export default class Keypad extends React.Component {
   handleKeyUp = () => {
+=======
+class Keypad extends React.Component {
+  handleKeyUp = (e) => {
+>>>>>>> a91c6a000985d5dc94b9be59bb01017f8bf27580
     console.log('Entering password...');
   }
 
   render() {
     return (
+<<<<<<< HEAD
         <input type="password" onKeyUp={this.handleKeyUp} />
+=======
+        <input type="password" onKeyUp={handleKeyUp} />
+>>>>>>> a91c6a000985d5dc94b9be59bb01017f8bf27580
     );
   }
 }
